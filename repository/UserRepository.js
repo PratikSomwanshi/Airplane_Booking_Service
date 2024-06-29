616b6dac02d02808e28e6b6d76ed6a20401be234
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2024-06-29 23:01:00 +0530

    userRepository path modified

--- repository/UserRepository.js
+++ repository/UserRepository.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const { StatusCodes } = require("http-status-codes");
 
 const User = require("../model/User");
-const CrudRepository = require("./CrudRepository");
+const CrudRepository = require("./crudrepository");
 
 class UserRepository extends CrudRepository {
     constructor() {
