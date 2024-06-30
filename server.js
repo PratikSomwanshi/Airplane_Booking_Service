8d9c917eea9a8238c6d9119dd3250b448d088830
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2024-06-30 17:34:02 +0530

    done with swagger ui interaction

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 var cors = require("cors");
+const oasTools = require("express-oas-generator");
 
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./route");
 const connectDB = require("./config/mongoDBConfig");
 
 const app = express();
+
+// http://localhost:8000/api-docs
+oasTools.init(app, {});
 
 app.use(cors());
 app.use(morgan(":method :url :status :response-time ms :date[web]"));
